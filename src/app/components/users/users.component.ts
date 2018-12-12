ad6e6533ee3a322351d7c3210e061dd7948b1548
Sam G. Zun <zunsamuel97@gmail.com>
2018-12-11 21:33:43 -0500

    working on section-4

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,20 +7,61 @@ import { User } from "src/app/models/User";
   styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
-  user: User;
+  users: User[];
+  showExtended: boolean = true;
+  loading: boolean = true;
+  enableAdd: boolean = false;
 
   constructor() {}
 
   ngOnInit() {
-    this.user = {
-      firstName: "Rimma",
-      lastName: "Zun",
-      age: 64,
-      address: {
-        street: "841 Ridge Road",
-        city: "Highland Park",
-        state: "IL"
-      }
-    };
+    setTimeout(() => {
+      this.users = [
+        {
+          firstName: "Rimma",
+          lastName: "Zun",
+          age: 64,
+          address: {
+            street: "841 Ridge Road",
+            city: "Highland Park",
+            state: "IL"
+          }
+        },
+        {
+          firstName: "Rimma",
+          lastName: "Zun",
+          age: 64,
+          address: {
+            street: "841 Ridge Road",
+            city: "Highland Park",
+            state: "IL"
+          }
+        },
+        {
+          firstName: "Rimma",
+          lastName: "Zun",
+          age: 64,
+          address: {
+            street: "841 Ridge Road",
+            city: "Highland Park",
+            state: "IL"
+          }
+        },
+        {
+          firstName: "Rimma",
+          lastName: "Zun"
+        }
+      ];
+      this.loading = false;
+    }, 2000);
+
+    // this.addUser({
+    //   firstName: "Rimma",
+    //   lastName: "Zun"
+    // });
+  }
+
+  addUser(user: User) {
+    this.users.push(user);
   }
 }
