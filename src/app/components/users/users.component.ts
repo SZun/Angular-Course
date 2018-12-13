44e7d0e5d9c6fd922050c12748f88498d20078c8
Sam G. Zun <zunsamuel97@gmail.com>
2018-12-13 00:21:36 -0500

    section-5 pre-cleanup

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -7,10 +7,20 @@ import { User } from "src/app/models/User";
   styleUrls: ["./users.component.css"]
 })
 export class UsersComponent implements OnInit {
+  user: User = {
+    firstName: "",
+    lastName: "",
+    age: null,
+    address: {
+      street: "",
+      city: "",
+      state: ""
+    }
+  };
   users: User[];
   showExtended: boolean = true;
   loading: boolean = true;
-  enableAdd: boolean = true;
+  enableAdd: boolean = false;
   showUserForm: boolean = true;
 
   constructor() {}
@@ -66,8 +76,21 @@ export class UsersComponent implements OnInit {
     }, 500);
   }
 
-  addUser(user: User) {
-    this.users.push(user);
+  addUser() {
+    this.user.isActive = true;
+    this.user.registered = new Date();
+
+    this.users.unshift(this.user);
+    this.user = {
+      firstName: "",
+      lastName: "",
+      age: null,
+      address: {
+        street: "",
+        city: "",
+        state: ""
+      }
+    };
   }
 
   toggleHide(user: User) {
