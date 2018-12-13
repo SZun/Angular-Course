b829d3ad9d91586c90c1b9f5fbf04a01add3cba8
Sam G. Zun <zunsamuel97@gmail.com>
2018-12-12 22:46:33 -0500

    section-4 pre-cleanub

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -10,7 +10,9 @@ export class UsersComponent implements OnInit {
   users: User[];
   showExtended: boolean = true;
   loading: boolean = true;
-  enableAdd: boolean = false;
+  enableAdd: boolean = true;
+  currentClasses = {};
+  currentStyles = {};
 
   constructor() {}
 
@@ -25,7 +27,11 @@ export class UsersComponent implements OnInit {
             street: "841 Ridge Road",
             city: "Highland Park",
             state: "IL"
-          }
+          },
+          image: "http://lorempixel.com/600/600/people/3",
+          isActive: true,
+          balance: 100,
+          registered: new Date("01/02/2018 08:30:00")
         },
         {
           firstName: "Rimma",
@@ -35,7 +41,11 @@ export class UsersComponent implements OnInit {
             street: "841 Ridge Road",
             city: "Highland Park",
             state: "IL"
-          }
+          },
+          image: "http://lorempixel.com/600/600/people/2",
+          isActive: false,
+          balance: 200,
+          registered: new Date("03/011/2017 07:20:00")
         },
         {
           firstName: "Rimma",
@@ -45,7 +55,11 @@ export class UsersComponent implements OnInit {
             street: "841 Ridge Road",
             city: "Highland Park",
             state: "IL"
-          }
+          },
+          image: "http://lorempixel.com/600/600/people/1",
+          isActive: true,
+          balance: 50,
+          registered: new Date("11/02/2016 10:30:00")
         },
         {
           firstName: "Rimma",
@@ -53,12 +67,24 @@ export class UsersComponent implements OnInit {
         }
       ];
       this.loading = false;
-    }, 2000);
+    }, 500);
 
-    // this.addUser({
-    //   firstName: "Rimma",
-    //   lastName: "Zun"
-    // });
+    this.setCurrentClasses();
+    this.setCurrentStyles();
+  }
+
+  setCurrentClasses() {
+    this.currentClasses = {
+      "btn-success": this.enableAdd,
+      "big-text": this.showExtended
+    };
+  }
+
+  setCurrentStyles() {
+    this.currentStyles = {
+      "padding-top": this.showExtended ? "0" : "40px",
+      "font-size": this.showExtended ? "40px" : "0"
+    };
   }
 
   addUser(user: User) {
